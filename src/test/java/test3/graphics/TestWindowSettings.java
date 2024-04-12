fce7f47293e48297172a4cb5c3d8132744af90de
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2024-04-12 14:21:07 +0300

    minor fix

--- src/test/java/test3/graphics/TestWindowSettings.java
+++ src/test/java/test3/graphics/TestWindowSettings.java
@@ -9,9 +9,9 @@ import commonGraphics.WindowSettings;
 
 //
 final class TestWindowSettings extends WindowSettings {
-    private static final @NotNull Dimension WINDOW_SIZE = new Dimension(400, 300);
+    private static final @NotNull Dimension WINDOW_SIZE = new Dimension(1100, 700);
     private static final @NotNull Point WINDOW_LOCATION = new Point(50, 50);
-    private static final @NotNull String WINDOW_TITLE = "Test 2";
+    private static final @NotNull String WINDOW_TITLE = "Test 3";
 
     //
     TestWindowSettings() {
